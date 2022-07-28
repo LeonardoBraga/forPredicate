cdbb7c3ad3f1db2778171a789a274d157603ba39
Leonardo Braga <leonardo.braga@gmail.com>
2022-07-28 01:41:22 -0400

    Initial code and basic documentation

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # forPredicate
+
+Small JavaScript Async helper that lets you await for the completion of a given predicate.
+
+## Usage:
+
+```
+await forPredicate(predicate, timeoutInSeconds = 10, checkIntervalInSeconds = 0.5);
+```
+
+## Parameters
+
+`predicate` is a function that is executed repeately until it returns a (https://developer.mozilla.org/en-US/docs/Glossary/Truthy)[`truthy`] value or the call times out. 
+
+`timeoutInSeconds` specifies the maximum duration, in seconds, within which the helper will try to execute the `predicate` before aborting the process, with a maximum value of 180 seconds and a minimum value of 0.1 seconds (100 milliseconds). 
+
+`checkIntervalInSeconds` the interval, in seconds, between each attempt to evaluate the `predicate` function, with a maximum value of 10 seconds and a minimum value of 0.1 seconds (100 milliseconds).
+
+I hope it helps!
+
+Cheers,
+Leonardo Braga
---
+++ forPredicate.js
@@ -0,0 +1,37 @@
+async function forPredicate(predicate, timeoutInSeconds = 10, checkIntervalInSeconds = 0.5) {
+  return new Promise((resolve, reject) => {
+    const timeoutInMilliseconds = Math.min(180, Math.max(0.1, timeoutInSeconds)) * 1000;
+    const checkIntervalMilliseconds = Math.min(10, Math.max(0.1, checkIntervalInSeconds)) * 1000;
+    const startTime = Date.now();
+
+    function checkPredicate() {
+      const predicateResult = predicate();
+      if (!predicateResult) {
+        if (startTime + timeoutInMilliseconds > Date.now()) {
+          setTimeout(checkPredicate, checkIntervalMilliseconds);
+        } else {
+          reject(new Error('Timeout'));
+        }
+      } else {
+        resolve(predicateResult);
+      }
+    }
+
+    checkPredicate();
+  });
+}
+
+let flag = false;
+const predicate = () => flag;
+
+// Completing the predicate asynchronously after 3s
+setTimeout(() => (flag = true), 3000);
+
+async function simpleTest() {
+  // awaiting for the predicate to be completed
+  const result = await forPredicate(predicate);
+
+  console.log(`Operation completed with result: ${result}`);
+}
+
+simpleTest();
